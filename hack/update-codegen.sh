0555e47109be8a24b5b8fd98a29cae3bcc5aa285
kahirokunn <okinakahiro@gmail.com>
2026-01-16 17:38:57 +0900

    Fix codegen script failure in CI
    Signed-off-by: kahirokunn <okinakahiro@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copyright 2024 The Kubernetes Authors.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,12 +18,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-set -o errexit
-set -o nounset
-set -o pipefail
+SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+CODEGEN_PKG="${CODEGEN_PKG:-}"
 
-SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+if [[ -z "${CODEGEN_PKG}" ]]; then
+    # Resolve from Go module cache
+    moddir="$(cd "${SCRIPT_ROOT}" && go list -m -f '{{.Dir}}' k8s.io/code-generator 2>/dev/null || true)"
+    CODEGEN_PKG="${moddir}"
+fi
+
+if [[ ! -f "${CODEGEN_PKG}/kube_codegen.sh" ]]; then
+    echo "error: kube_codegen.sh not found at ${CODEGEN_PKG}" >&2
+    exit 1
+fi
 
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
